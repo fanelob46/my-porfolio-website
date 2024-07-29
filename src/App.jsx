6c59b80f317033d401a8ce32a31e170d7cbdf05e
fanelob46 <fanelob46@gmail.com>
2024-07-29 15:36:25 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Home from './Componets/Home/Home'
+import Portfolio from './Componets/Portfolio/Portfolio'
+import Info from './Componets/Information/Info'
+import Footer from './Componets/Footer/Footer'
+
+const App = () => {
+  return (
+    <>
+    <Home/>
+    <Portfolio/>
+    <Info/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Componets/Information/Info.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Info.css'
+
+const Info = () => {
+  return (
+    <div className='info'>
+      <div className="discuss">
+            <h1>Lets discuss</h1>
+            <h1>Somethings <span>Cool</span></h1>
+            <h1>Together</h1>
+        </div>
+        <div className="email-info">
+            <div className="name">
+            <input type="text" placeholder="Your Name" name="text"></input>
+            </div>
+            <div className="email">
+            <input type="email" placeholder="Your Email" name="email"></input>
+            </div>
+            <div className="message">
+            <input type="text" placeholder="Your Message" name="text"></input>
+            </div>
+        <button className="btn-sub">Submit</button>
+        </div>
+    </div>
+  )
+}
+
+export default Info
